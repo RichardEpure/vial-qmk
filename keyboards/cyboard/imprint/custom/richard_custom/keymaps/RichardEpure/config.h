fe89a8776b59d1723c215846ab2087d77ec0d199
Richard <43828146+RichardEpure@users.noreply.github.com>
2026-01-07 22:11:24 +0000

    add custom keymap

---
+++ keyboards/cyboard/imprint/custom/richard_custom/keymaps/RichardEpure/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#    define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+#endif
+
+#ifndef PERMISSIVE_HOLD_PER_KEY
+#    define PERMISSIVE_HOLD_PER_KEY
+#endif
+
+#ifndef RETRO_TAPPING_PER_KEY
+#    define RETRO_TAPPING_PER_KEY
+#endif
--- quantum/qmk_settings.c
+++ quantum/qmk_settings.c
@@ -289,21 +289,21 @@ uint16_t qs_get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     return QS.tapping_term;
 }
 
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-    return QS_tapping_permissive_hold;
-}
+/* bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) { */
+/*     return QS_tapping_permissive_hold; */
+/* } */
 
-bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-    return QS_tapping_hold_on_other_key_press;
-}
+/* bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) { */
+/*     return QS_tapping_hold_on_other_key_press; */
+/* } */
 
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     return QS.quick_tap_term;
 }
 
-bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
-    return QS_tapping_retro_tapping;
-}
+/* bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) { */
+/*     return QS_tapping_retro_tapping; */
+/* } */
 
 uint16_t get_combo_term(uint16_t index, combo_t *combo) {
     return QS.combo_term;
