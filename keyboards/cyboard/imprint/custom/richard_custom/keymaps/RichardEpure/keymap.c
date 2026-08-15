17e4c80f24efff9e261f36e9a6dfb328e90ab509
Richard <43828146+RichardEpure@users.noreply.github.com>
2026-08-15 22:20:34 +0100

    Update keybinds

--- keyboards/cyboard/imprint/custom/richard_custom/keymaps/RichardEpure/keymap.c
+++ keyboards/cyboard/imprint/custom/richard_custom/keymaps/RichardEpure/keymap.c
@@ -87,7 +87,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_fun_custom(
-        KC_NO,       KC_F1,                    KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                     KC_F11,
+        TG(_GAMING_OTHER), KC_F1,                    KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                     KC_F11,
         KC_NO,       KC_1,                     KC_2,  KC_3,  KC_4,  KC_5,  KC_6,                                      KC_6,  KC_7,  KC_8,  KC_9,  KC_0,                       KC_F12,
         KC_ESC,      KC_Q,                     KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,                                      KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,                       KC_NO,
         KC_TAB,      KC_A,                     KC_S,  KC_D,  KC_F,  KC_G,  KC_H,                                      KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,                    KC_QUOT,
@@ -110,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUMBERS] = LAYOUT_fun_custom(
         _______, _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, KC_NUM,  KC_PSLS, KC_PAST, KC_PEQL, KC_NO,   KC_NO,                             KC_NUM,  KC_PSLS, KC_PAST, KC_PEQL, KC_NO,   _______,
-        _______, KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_NO,                             KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
-        _______, KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_NO,                             KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
-        _______, KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_NO,                                     KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_NO,   _______,
+        _______, KC_NUM,  KC_PEQL, KC_PAST, KC_PSLS, KC_NO,   KC_NO,                             KC_NUM,  KC_PSLS, KC_PAST, KC_PEQL, KC_NO,   _______,
+        _______, KC_PMNS, KC_P7,   KC_P8,   KC_P9,   KC_NO,   KC_NO,                             KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
+        _______, KC_PPLS, KC_P4,   KC_P5,   KC_P6,   KC_P0,   KC_NO,                             KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
+        _______, KC_LCTL, KC_P1,   KC_P2,   KC_P3,   KC_PDOT,                                   KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_LCTL, _______,
 
                                 _______, _______,    KC_BSPC, KC_ENT, TG(_NUMBERS),    KC_SPC, KC_ENT, KC_BSPC,    _______, _______,
                                                 LT(8, KC_PDOT), KC_P0, KC_DEL, KC_SPC,    TO(_BASE), KC_DEL, KC_0, LT(8, KC_PDOT)
@@ -153,14 +153,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_GAMING_OTHER] = LAYOUT_fun_custom(
+        _______,  _______, KC_7,    KC_8,    KC_9,    KC_0,    _______,                           _______, _______, _______, _______,  _______, _______,
         _______,  _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______,  _______, _______,
         _______,  _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______,  _______, _______,
-        ESC_TYPE, _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______,  _______, _______,
         _______,  _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______,  _______, _______,
-        _______,  _______, _______, _______, _______, _______,                                    _______, _______, _______, _______, ENT_TYPE, _______,
+        _______,  _______, _______, _______, _______, _______,                                    _______, _______, _______, _______, _______,  _______,
 
-                                _______, _______,    _______, _______, _______,    ENT_TYPE, _______, _______,    _______, _______,
-                                                _______, _______, TO(_BASE), _______,    _______, _______, _______, _______
+                                _______, _______,    LCTL(KC_LSFT), KC_SPC, LSFT(KC_DEL),    _______, _______, _______,    _______, _______,
+                                                KC_LCTL, _______, _______, _______,    _______, _______, _______, _______
     ),
 
     [7] = LAYOUT_fun_custom(
